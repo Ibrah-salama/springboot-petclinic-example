70e8f9dfbcec419c6ea9fb7eb531c4c0cef8d7a2
Ibrahim Mohamed <ibrah.salama@outlook.com>
2022-04-09 01:11:18 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinicv2/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicv2.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/sfgpetclinicv2/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicv2.model;
+
+public class Vet extends Person {
+}
