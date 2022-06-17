b0d314c083214dd28171d09fa05fe9bbb45e9fad
Ibrahim Mohamed <ibrah.salama@outlook.com>
2022-06-18 00:30:45 +0200

    Create Visit Map service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv2/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicv2.services;
+
+import guru.springframework.sfgpetclinicv2.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
